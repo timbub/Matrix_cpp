97e16e0fe6d332a243d762c10825c91999e93d44
Tim <timasurckov@gmail.com>
2026-02-27 11:22:32 +0300

    add unit tests

--- tests/unit/tests.cpp
+++ tests/unit/tests.cpp
@@ -2,10 +2,111 @@
 #include "../../Matrix.hpp"
 #include "gtest/gtest.h"
 
+//namespace {
+//    struct ThrowingElement {
+//        double value_;
+//        static inline int current_copies_ = 0;
+//        static inline int throw_after = -1;
+//
+//        static void reset() {
+//            current_copies_ = 0;
+//            throw_after = -1;
+//        }
+//
+//        explicit ThrowingElement(int value) : value_(value) {}
+//
+//        ThrowingElement(const ThrowingElement& other) : value_(other.value_) {
+//            if (throw_after != -1 && ++current_copies_ >= throw_after) throw std::bad_alloc();
+//        }
+//
+//        auto operator <=>(const ThrowingElement& other) const = default;
+//
+//    };
+//};
+
 TEST (BUFFER, move_ctor) {
+    matrix::BufMatrix<int> buf1(3);
+    buf1[0] = 5;
+    buf1[1] = 10;
+    auto buf2(std::move(buf1));
+    EXPECT_EQ(buf2[0] + buf2[1], 15);
+}
+
+TEST (BUFFER, move_assigments) {
     matrix::BufMatrix<int> buf1(3);
     buf1[0] = 5;
     buf1[1] = 10;
     auto buf2 = std::move(buf1);
     EXPECT_EQ(buf2[0] + buf2[1], 15);
 }
+
+TEST (MATRIX, copy_ctor) {
+    matrix::Matrix<double> matrix1{3,3};
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            matrix1[i][j] = i;
+        }
+    }
+    auto matrix2(matrix1);
+    EXPECT_EQ(matrix1.determinate(), matrix2.determinate());
+}
+
+TEST (MATRIX, move_ctor) {
+    matrix::Matrix<double> matrix1{3,3};
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            matrix1[i][j] = i;
+        }
+    }
+    double expected = matrix1.determinate();
+    auto matrix2(std::move(matrix1));
+    EXPECT_EQ(expected, matrix2.determinate());
+}
+
+TEST (MATRIX, copy_assigment) {
+    matrix::Matrix<double> matrix1{3,3};
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            matrix1[i][j] = i;
+        }
+    }
+    auto matrix2 = matrix1;
+    EXPECT_EQ(matrix1.determinate(), matrix2.determinate());
+}
+
+TEST (MATRIX, move_assigment) {
+    matrix::Matrix<double> matrix1{3,3};
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            matrix1[i][j] = i;
+        }
+    }
+    double expected = matrix1.determinate();
+    auto matrix2 = std::move(matrix1);
+    EXPECT_EQ(expected, matrix2.determinate());
+}
+
+//TEST (EXCEPTION, copy_ctor) {
+//    ThrowingElement::reset();
+//
+//    matrix::Matrix<ThrowingElement> matrix{3,3};
+//    for (int i = 0; i < 3; i++) {
+//        for (int j = 0; j < 3; j++) {
+//            matrix[i][j] = ThrowingElement(i);
+//        }
+//    }
+//
+//    size_t answer_before = matrix.determinate();
+//
+//    ThrowingElement::reset();
+//    ThrowingElement::throw_after = 3;
+//
+//    EXPECT_THROW(matrix::Matrix<ThrowingElement> matrix2(matrix), std::bad_alloc);
+//
+//    ThrowingElement::reset();
+//    size_t answer_after = matrix.determinate();
+//
+//    EXPECT_EQ(answer_after, answer_before);
+//}
+
+
